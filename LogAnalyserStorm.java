36d185d4491a2f6586b680954ad0f7063b92119a
Vikas Taank <vikas.taank@gmail.com>
2017-05-16 12:28:14 +0530

    A storm tolology cluster

---
+++ LogAnalyserStorm.java
@@ -0,0 +1,34 @@
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+
+//import storm configuration packages
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+
+//Create main class LogAnalyserStorm submit topology.
+public class LogAnalyserStorm {
+   public static void main(String[] args) throws Exception{
+      //Create Config instance for cluster configuration
+      Config config = new Config();
+      config.setDebug(true);
+		
+      //
+      TopologyBuilder builder = new TopologyBuilder();
+      builder.setSpout("call-log-reader-spout", new FakeCallLogReaderSpout());
+
+      builder.setBolt("call-log-creator-bolt", new CallLogCreatorBolt())
+         .shuffleGrouping("call-log-reader-spout");
+
+      builder.setBolt("call-log-counter-bolt", new CallLogCounterBolt())
+         .fieldsGrouping("call-log-creator-bolt", new Fields("call"));
+			
+      LocalCluster cluster = new LocalCluster();
+      cluster.submitTopology("LogAnalyserStorm", config, builder.createTopology());
+      Thread.sleep(10000);
+		
+      //Stop the topology
+		
+      cluster.shutdown();
+   }
+}
